6ca0c9f92f4e11b0bc3cdb421f6431508722db94
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2025-12-02 16:55:34 +0300

    Returned project and app structure to SQLite for testing before switching to MySQL

--- Django_version/placeholder_project/placeholder_project/asgi.py
+++ Django_version/placeholder_project/placeholder_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for onboard_test project.
+ASGI config for placeholder_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onboard_test.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'placeholder_project.settings')
 
 application = get_asgi_application()
